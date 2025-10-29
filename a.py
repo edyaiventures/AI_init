68345e239ae359fd555e4e34fc4224e0dfd530f2
HQ-H0JPKQY9PL <Venzel13@gmail.com>
2025-10-29 19:11:49 +0300

    add openai api package

--- a.py
+++ a.py
@@ -1,2 +1,13 @@
 import langchain
 import langgraph
+import openai
+
+
+import os
+from openai import OpenAI
+
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key=os.getenv("OPENAI_API_KEY"),
+)
+print(client)
